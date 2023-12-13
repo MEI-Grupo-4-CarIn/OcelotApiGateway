9ca9ff35a53c0193f7ce45d3027a95e2de978264
Duarte Fernades <duartejose99@hotmail.com>
2023-12-13 16:25:12 +0000

    wip++++

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace OcelotApiGateway
 {
@@ -12,6 +15,12 @@ namespace OcelotApiGateway
     {
         public static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            var config = builder.Build();
+
             new WebHostBuilder()
             .UseKestrel()
             .UseContentRoot(Directory.GetCurrentDirectory())
@@ -21,15 +30,31 @@ namespace OcelotApiGateway
                     .AddJsonFile("ocelot.json")
                     .AddEnvironmentVariables();
             })
-            .ConfigureServices(s => {
+            .ConfigureServices(s =>
+            {
                 s.AddHttpClient();
                 s.AddOcelot();
-                s.AddSingleton<JwtValidationMiddleware>();
+                s.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer("JwtAuthentication", options =>
+                    {
+                        var rsa = new RSACryptoServiceProvider();
+                        rsa.FromXmlString(config["JwtSettings:PublicKey"]);
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = config["JwtSettings:Issuer"],
+                            ValidAudience = config["JwtSettings:Audience"],
+                            IssuerSigningKey = new RsaSecurityKey(rsa.ExportParameters(false))
+                        };
+                    });
             })
             .UseIISIntegration()
             .Configure(app =>
             {
-                app.UseMiddleware<JwtValidationMiddleware>();
+                app.UseAuthentication();
                 app.UseOcelot().Wait();
             })
             .Build()
